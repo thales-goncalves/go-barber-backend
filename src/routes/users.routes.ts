2e8a9959ae86d368bb6d58bdafb4b7fac4f1dc10
Thales Gonçalves <thales.g.goncalves@outlook.com>
2020-06-25 16:45:59 -0300

    upload images

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserSerivce';
+import UpdateUserAvatarService from '../services/UploadUserAvatarService';
+
+import ensureAuthenticate from '../middlewares/ensureAuthenticate';
+import uploadConfig from '../config/upload';
 
 const userRoutes = Router();
+const upload = multer(uploadConfig);
 
 userRoutes.post('/', async (request, response) => {
   try {
@@ -23,5 +29,27 @@ userRoutes.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+userRoutes.patch(
+  '/avatar',
+  ensureAuthenticate,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateAvatar = new UpdateUserAvatarService();
+
+      const user = await updateAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default userRoutes;
